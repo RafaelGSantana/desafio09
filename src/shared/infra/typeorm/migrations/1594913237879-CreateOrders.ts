e2335ceab25ac41a9cb55e4ba57c9dce1bb3dd93
Rafael Santana <rgs1990419@gmail.com>
2020-07-16 15:29:51 -0300

    Migrations

---
+++ src/shared/infra/typeorm/migrations/1594913237879-CreateOrders.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrders1594913237879 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'created-at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated-at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders');
+  }
+}
